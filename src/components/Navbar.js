a1884979e03bb0c0cc2da4ae66c0cd2ba09e9982
afbelardi <afbelardi@gmail.com>
2023-02-14 14:54:36 -0500

    title spacing tweak

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import styles from '../styles/navbar.module.css';
 export default function Navbar () {
     return (
         <div className=" h-1/6">
-            <div className='p-4'>
-            <h1 className="text-4xl text-white font-bebas">Alex</h1>
-            <h1 className="text-4xl text-white font-bebas">Belardi</h1>
+            <div className='p-5'>
+            <h1 className="-my-2 text-4xl text-white font-bebas">Alex</h1>
+            <h1 className="-my-2 text-4xl text-white font-bebas">Belardi</h1>
             <h1 className="text-4xl text-white font-bebas">Code</h1>
             </div>
         </div>
