adc07806819ec7f38c4210732a98e54ba8404483
afbelardi <afbelardi@gmail.com>
2023-02-17 12:53:21 -0500

    changed animation for info

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -6,8 +6,11 @@ export default function Splash (props) {
         <Fade bottom opposite className="opacity-5">
             <div className="absolute top-0 z-40 flex w-screen h-screen bg-black opacity-40">
                 <section className="flex flex-col items-baseline justify-end w-full mt-36 ml-11 h-1/3">
+                    <Fade left delay={400}>
                     <p className="mb-2 font-bold text-gray-50">Alex Belardi</p>
-                    <p className="text-gray-50">Software Developer</p>
+                    <p className="mb-2 text-gray-50">Software Developer</p>
+                    <a className="underline text-gray-50" href="mailto:afbelardicode@gmail.com">afbelardicode@gmail.com</a>
+                    </Fade>
                 </section>
             </div>
        </Fade>
