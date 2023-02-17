49b0a8d4782643ea68f5be021e80ae7fc9229151
afbelardi <afbelardi@gmail.com>
2023-02-17 18:51:28 -0500

    name and title with new font and sizing

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -8,9 +8,9 @@ export default function Splash (props) {
             <div className="absolute top-0 z-40 flex flex-col w-screen h-screen bg-black opacity-40">
                 <section className="flex flex-col justify-end w-1/2 overflow-hiddenitems-baseline mt-36 ml-11 h-1/3">
                     <Fade left delay={400}>
-                        <p className="mb-1 text-2xl font-bold text-gray-50 font-recoleta-black">Alex Belardi</p>
-                        <p className="mb-2 text-gray-50 font-sabon">Software Developer</p>
-                        <a className="underline text-gray-50 font-sabon" href="mailto:afbelardicode@gmail.com">afbelardicode@gmail.com</a>
+                        <p className="text-3xl font-bold  text-gray-50 font-recoleta-black">Alex Belardi</p>
+                        <p className="text-xl text-gray-50 font-recoleta-light">Software Developer</p>
+                        <a className="text-lg underline text-gray-50 font-recoleta-light" href="mailto:afbelardicode@gmail.com">afbelardicode@gmail.com</a>
                     </Fade>
                 </section>
                 <section className="flex items-center justify-center w-full h-1/6">
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
     fontFamily: {
       'bebas': ['Bebas Neue', 'sans-serif'],
       'sabon': ['Sabon Bold', 'sans-serif'],
-      'recoleta-black': ['Recoleta Black', 'sans-serif']
+      'recoleta-black': ['Recoleta Black', 'sans-serif'],
+      'recoleta-light': ['Recoleta Light', 'sans-serif']
     }
   },
   plugins: [],
