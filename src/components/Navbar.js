d9c4416ff714b56269449f12a8da7abcf10354cd
afbelardi <afbelardi@gmail.com>
2023-02-17 18:59:26 -0500

    got rid of unneccessary function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,19 +5,15 @@ import { Divide as Hamburger } from 'hamburger-react';
 
 export default function Navbar(props) {
 
-    const clickingFunction = () => {
-        console.log('testing')
-    }
-    
     return (
         <div className="z-0 flex justify-between h-1/8">
-            <div className='z-50 p-5'>
+            <div className='z-40 p-5'>
                 <h1 className="-my-2 text-4xl text-white font-bebas">Alex</h1>
-                <h1 className="-my-2 text-4xl text-white  font-bebas">Belardi</h1>
-                <h1 className="text-4xl text-white  font-bebas">Code</h1>
+                <h1 className="-my-2 text-4xl text-white font-bebas">Belardi</h1>
+                <h1 className="text-4xl text-white font-bebas">Code</h1>
             </div>
             <div className="z-50 flex justify-center w-1/4 mt-8">
-                <Hamburger onClick={clickingFunction} toggled={props.isOpen} toggle={props.setIsOpen} color="white" size={40}/>
+                <Hamburger toggled={props.isOpen} toggle={props.setIsOpen} color="white" size={40}/>
             </div>
         </div>
     )
