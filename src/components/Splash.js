899effc8a6adea0fd006408fa967d66372c8172d
afbelardi <afbelardi@gmail.com>
2023-02-17 19:08:32 -0500

    buttons added, colors of font on splash screen changed

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -6,15 +6,18 @@ export default function Splash (props) {
     return (
         <Fade bottom opposite className="opacity-5">
             <div className="absolute top-0 z-40 flex flex-col w-screen h-screen bg-black opacity-40">
-                <section className="flex flex-col justify-end w-1/2 overflow-hiddenitems-baseline mt-36 ml-11 h-1/3">
+                <section className="flex flex-col items-baseline justify-end w-1/2 overflow-hidden mt-36 ml-11 h-1/3">
                     <Fade left delay={400}>
-                        <p className="text-3xl font-bold  text-gray-50 font-recoleta-black">Alex Belardi</p>
-                        <p className="text-xl text-gray-50 font-recoleta-light">Software Developer</p>
-                        <a className="text-lg underline text-gray-50 font-recoleta-light" href="mailto:afbelardicode@gmail.com">afbelardicode@gmail.com</a>
+                        <p className="text-3xl font-bold text-blue-100 font-recoleta-black">Alex Belardi</p>
+                        <p className="text-xl text-blue-100 font-recoleta-light">Software Developer</p>
+                        <a className="text-lg text-blue-100 underline font-recoleta-light" href="mailto:afbelardicode@gmail.com">afbelardicode@gmail.com</a>
                     </Fade>
                 </section>
-                <section className="flex items-center justify-center w-full h-1/6">
-                    <FaGithub className={styles.icons}/>
+                <section className="flex items-center w-full justify-evenly h-1/6">
+                    <button className="p-6 bg-gray-300 rounded-3xl h-11 w-11"></button>
+                    <button className="p-6 bg-gray-300 rounded-3xl h-11 w-11"></button>
+                    <button className="p-6 bg-gray-300 rounded-3xl h-11 w-11"></button>
+                    <button className="p-6 bg-gray-300 rounded-3xl h-11 w-11"></button>
                 </section>
             </div>
        </Fade>
