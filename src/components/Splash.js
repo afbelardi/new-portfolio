3f9f5c507f3abce2d9e5df1b8e008274c8ae62bb
afbelardi <afbelardi@gmail.com>
2023-02-20 17:14:08 -0500

    changed background colors of main page and menu

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -5,7 +5,7 @@ import { FaGithub, FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa'
 export default function Splash (props) {
     return (
         <Fade bottom opposite className="opacity-5">
-            <div className="absolute top-0 z-40 flex flex-col w-screen h-screen bg-black opacity-40">
+            <div className="absolute top-0 z-40 flex flex-col w-screen h-screen bg-light-navy opacity-40">
                 <section className="flex flex-col items-baseline justify-end w-2/3 overflow-hidden mt-36 ml-11 h-1/3">
                     <Fade left delay={400}>
                         <p className="text-3xl font-bold text-blue-100 font-recoleta-black">Alex Belardi</p>
@@ -16,7 +16,7 @@ export default function Splash (props) {
                 <Fade right delay={400}>
                     <section className="flex items-center w-full justify-evenly h-1/6">
                         <a href="https://www.github.com/afbelardi" target="_blank"  className="flex items-center justify-center bg-gray-200 rounded-3xl h-11 w-11">
-                            <FaGithub size={28}/>
+                            <FaGithub size={28} />
                         </a>
                         <a href="https://www.linkedin.com/in/afbelardi" target="_blank" className="flex items-center justify-center bg-gray-200 rounded-3xl h-11 w-11">
                             <FaLinkedin size={28} />
@@ -38,6 +38,5 @@ export default function Splash (props) {
                 </Fade>
             </div>
        </Fade>
-
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navy': '#0a192f',
+        'light-navy': '#112240'
+      }
+    },
     fontFamily: {
       'bebas': ['Bebas Neue', 'sans-serif'],
       'sabon': ['Sabon Bold', 'sans-serif'],
       'recoleta-black': ['Recoleta Black', 'sans-serif'],
       'recoleta-light': ['Recoleta Light', 'sans-serif']
-    }
+    },
+    
   },
   plugins: [],
 }
