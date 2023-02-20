022a3e8873c4f4ad450acc7bbd434e2ac9415a6a
afbelardi <afbelardi@gmail.com>
2023-02-20 18:16:19 -0500

    about me title

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,15 @@
 import styles from '../styles/aboutme.module.css';
+import { Fade } from 'react-reveal';
 
 export default function AboutMe(props) {
     return(
-        <div className="flex w-full p-6 mt-14 h-1/4" id={styles.about}>
-            <h1 className="text-4xl text-white font-recoleta-bold">About Me</h1>
+        <div className="flex w-full p-6 mt-14 h-1/4" >
+            <div className="flex w-full">
+                <h1 className="text-3xl text-white font-recoleta-bold" id={styles.about}>About Me</h1>
+            <Fade right>
+                <hr className="w-2/5 h-0 mt-5 bg-white opacity-60 ml-7" />
+            </Fade>
+            </div>
         </div>
     )
 }
