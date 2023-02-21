5a2c79bb1f694f39505fdb318693e1d64f5edd7c
afbelardi <afbelardi@gmail.com>
2023-02-21 17:53:34 -0500

    minor changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,14 +3,16 @@ import { Fade } from 'react-reveal';
 
 export default function AboutMe(props) {
     return(
-        <div className="flex flex-col w-full p-6 mt-10 h-2/3">
+        <div className="flex flex-col w-full p-8 mt-10 h-2/3">
             <div className="flex w-full">
-                <h1 className="text-4xl text-white font-recoleta-bold" id={styles.about}>About Me</h1>
+                <h1 className="ml-4 text-4xl text-white font-recoleta-bold" id={styles.about}>About Me</h1>
             <Fade right>
                 <hr className="w-2/5 h-0 mt-5 bg-lighter-blue ml-7" />
             </Fade>
             </div>
-            <img src='/me.jpg' id={styles.about} className="mt-5 rounded-lg"></img>
+            <section className="flex justify-center">
+            <img src='/me.jpg' id={styles.about} className="w-full mt-5 rounded-lg h-10/12"></img>
+            </section>
         </div>
     )
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,8 @@
-
+import styles from '../styles/aboutme.module.css';
 
 export default function Intro() {
     return(
-        <div className="flex flex-col w-full h-52 mt-14">
+        <div className="flex flex-col w-full h-52 mt-14" id={styles.about}>
             <section className="flex flex-col w-full">
                 <h1 className="p-2 ml-6 text-lg text-white font-recoleta-regular">Hi, welcome to my portfolio.</h1>
                 <h1 className="mb-2 ml-8 text-2xl text-blue-100 font-recoleta-bold">My name is Alex Belardi.</h1>
