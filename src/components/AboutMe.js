b92fc4fb9ca0178f45fa2f05c797516df7576ddd
afbelardi <afbelardi@gmail.com>
2023-02-22 11:18:12 -0500

    small paragraph updates

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,7 @@ import { Fade } from 'react-reveal';
 
 export default function AboutMe(props) {
     return(
-        <div className="flex flex-col w-full p-6 mt-10 min-h-400">
+        <div className="flex flex-col w-full p-4 mt-10 min-h-400">
             <div className="flex w-full">
                 <h1 className="ml-2 text-4xl text-white font-recoleta-bold" id={styles.about}>About Me</h1>
                 <Fade right>
@@ -12,8 +12,8 @@ export default function AboutMe(props) {
             </div>
             <section className="flex flex-col items-center justify-center h-5/6">
                 <img src='/me.jpg' id={styles.about} className="w-full mt-5 rounded-lg min-h-5/6 max-w-400 max-h-400"></img>
-                <main className="flex flex-col items-center w-5/6 mt-4 max-w-500">
-                <p className="text-lg text-white font-recoleta-light">My interest in technology started from a young age. By 13 years old, I was creating
+                <main className="w-full mt-4 text-center max-w-500">
+                <p className="text-white text-md font-recoleta-light">My interest in technology started from a young age. By 13 years old, I was creating
                 </p>
             </main>
             </section>
