16409b30d506f797052663ad5bb664973b8295b7
afbelardi <afbelardi@gmail.com>
2023-02-22 11:36:28 -0500

    added more info to about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,7 +13,11 @@ export default function AboutMe(props) {
             <section className="flex flex-col items-center justify-center h-5/6">
                 <img src='/me.jpg' id={styles.about} className="w-full mt-5 rounded-lg min-h-5/6 max-w-400 max-h-400"></img>
                 <main className="w-full mt-4 text-center max-w-500">
-                <p className="text-white text-md font-recoleta-light">My interest in technology started from a young age. By 13 years old, I was creating
+                <p className="text-white text-md font-recoleta-light">My interest in technology started from a young age. By 13 years old, I was creating technology product review videos for my
+                YouTube channel. While partnering with major brands, I had amassed a following of 7,000 subscribers and 2 million video views.
+                </p>
+                <p className="mt-2 text-white text-md font-recoleta-light">After graduating college, I started a job at an Apple retail store and worked my up to the title of Genius. During that
+                time, I decided to start learning how to code and attended General Assembly's Software Engineering Immersive bootcamp. 
                 </p>
             </main>
             </section>
