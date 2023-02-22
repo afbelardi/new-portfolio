3f3fe096964164c10bbf2b7948346161e731fe80
afbelardi <afbelardi@gmail.com>
2023-02-22 11:08:21 -0500

    fixed sizing of about me photo

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,16 +3,21 @@ import { Fade } from 'react-reveal';
 
 export default function AboutMe(props) {
     return(
-        <div className="flex flex-col w-full p-8 mt-10 h-2/3">
+        <div className="flex flex-col w-full p-6 mt-10 min-h-400">
             <div className="flex w-full">
-                <h1 className="ml-4 text-4xl text-white font-recoleta-bold" id={styles.about}>About Me</h1>
-            <Fade right>
-                <hr className="w-2/5 h-0 mt-5 bg-lighter-blue ml-7" />
-            </Fade>
+                <h1 className="ml-2 text-4xl text-white font-recoleta-bold" id={styles.about}>About Me</h1>
+                <Fade right>
+                    <hr className="w-2/5 h-0 mt-5 bg-lighter-blue ml-7" />
+                </Fade>
             </div>
-            <section className="flex justify-center">
-            <img src='/me.jpg' id={styles.about} className="w-full mt-5 rounded-lg h-10/12"></img>
+            <section className="flex flex-col items-center justify-center h-5/6">
+                <img src='/me.jpg' id={styles.about} className="w-full mt-5 rounded-lg min-h-5/6 max-w-400 max-h-400"></img>
+                <main className="flex flex-col items-center w-5/6 mt-4 max-w-500">
+                <p className="text-lg text-white font-recoleta-light">My interest in technology started from a young age. By 13 years old, I was creating
+                </p>
+            </main>
             </section>
+            
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ export default function Home(props) {
       />
       <Intro />
       <AboutMe />
-      <div className="h-56 bg-white">
+      <div className="h-56 bg-white mt-11">
 
       </div>
       {isOpen ? 
