9c026b7dd7edd0bdf52d9e0448d5439d8540107e
afbelardi <afbelardi@gmail.com>
2023-02-22 15:44:50 -0500

    font color intro change

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,8 +5,8 @@ export default function Intro() {
         <div className="flex flex-col w-full h-52 mt-14" id={styles.about}>
             <section className="flex flex-col w-full">
                 <h1 className="p-2 ml-6 text-lg text-white font-recoleta-regular">Hi, welcome to my portfolio.</h1>
-                <h1 className="mb-2 ml-8 text-2xl text-blue-100 font-recoleta-bold">My name is Alex Belardi.</h1>
-                <h1 className="w-4/5 ml-8 text-2xl text-blue-100 font-recoleta-bold">I'm passionate about turning cool and innovative ideas into web apps.</h1>
+                <h1 className="mb-2 ml-8 text-2xl text-blue-200 font-recoleta-bold">My name is Alex Belardi.</h1>
+                <h1 className="w-4/5 ml-8 text-2xl text-blue-300 font-recoleta-bold">I'm passionate about turning cool and innovative ideas into web apps.</h1>
             </section>
         </div>
     )
