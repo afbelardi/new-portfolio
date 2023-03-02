68608ce6b015d9bfc31c1451ac21f792d5c31390
afbelardi <afbelardi@gmail.com>
2023-03-02 12:58:30 -0500

    icon for projects added and styled

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,17 +21,26 @@ export default function Projects(props) {
                         <img className="rounded-lg opacity-15"  src="/photoblog.png" />
                         <div className="absolute top-0 left-0 z-40 w-full h-full opacity-90 bg-light-navy">
                             <div className="w-full h-full p-6">
-                                <VscFolderOpened />
+                                <VscFolderOpened size={40} className="text-blue-300"/>
                             </div>
                         </div>
                 </div>
                 <div ref={nftRef} className={`${styles.notanimated} ${isNftVisible ? styles.projects: ''}`}>
                         <img className="rounded-lg opacity-15"  src="/photomint.png" />
-                        <div className="absolute top-0 left-0 z-40 w-full h-full opacity-90 bg-light-navy"></div>
+                        <div className="absolute top-0 left-0 z-40 w-full h-full opacity-90 bg-light-navy">
+                            <div className="w-full h-full p-6 bg-light-navy">
+                                <VscFolderOpened size={40} className="text-blue-300" />
+                            </div>
+                        </div>
+                       
                 </div>
                 <div ref={gtopRef} className={`${styles.notanimated} ${isGtopVisible ? styles.projects: ''}`}>
                         <img className="rounded-lg "  src="/gtop.png" />
-                        <div className="absolute top-0 left-0 z-40 w-full h-full bg-light-navy opacity-90"></div>
+                        <div className="absolute top-0 left-0 z-40 w-full h-full bg-light-navy opacity-90">
+                            <div className="w-full h-full p-6">
+                                <VscFolderOpened size={40} className="text-blue-300"/>
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
