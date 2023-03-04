2485fed5851eebf597df7382526bb24401c008ec
afbelardi <afbelardi@gmail.com>
2023-03-04 14:53:05 -0500

    squares added in for skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,6 +11,15 @@ export default function Skills(props) {
                     <hr className="w-2/5 h-0 mt-5 bg-lighter-blue ml-7" />
                 </Fade>
             </div>
+            <main className="flex w-full mt-7">
+                <ul className="flex flex-col items-center w-full">
+                    <li className="w-2/3 h-48 bg-blue-300 rounded-t-lg max-w-400">React</li>
+                    <li className="w-2/3 h-48 bg-blue-400 max-w-400">JavaScript</li>
+                    <li className="w-2/3 h-48 bg-blue-500 max-w-400">CSS</li>
+                    <li className="w-2/3 h-48 bg-blue-600 max-w-400">Express</li>
+                    <li className="w-2/3 h-48 bg-blue-700 rounded-b-lg max-w-400">Solidity</li>
+                </ul>
+            </main>
         </div>
     )
 }
