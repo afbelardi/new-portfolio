54b70710ccbac43c14141b05327f1a494ab70e2e
afbelardi <afbelardi@gmail.com>
2023-03-06 19:38:52 -0500

    sidebar links completed

--- src/components/Sidebars.js
+++ src/components/Sidebars.js
@@ -1,6 +1,16 @@
-
+import { BiCaretUp } from 'react-icons/bi';
+import { VscGithubAlt } from 'react-icons/vsc';
+import { CiTwitter, CiLinkedin, CiInstagram } from 'react-icons/ci';
+import { SlSocialLinkedin, SlSocialInstagram } from 'react-icons/sl';
+import { IoNewspaperOutline } from 'react-icons/io5';
 
 export default function Sidebars() {
+
+    // const caretList = () => {
+    //     return ()
+    // }
+
+    
     return (
         <>
             <div className="hidden md:w-13 md:bottom-0 md:flex-col md:right-0 md:flex md:items-center md:mb-2 md:mr-5 md:fixed md:h-96 md:">
@@ -16,12 +26,23 @@ export default function Sidebars() {
                 <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
                 <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
             </div>
-            <div className="hidden md:justify-center md:ml-5 md:items-center md:w-11 md:flex-col md:flex md:bottom-0 md:left-0 md:fixed md:h-96">
-            <div className="flex flex-col items-end h-2/5">
-                <VscGithubAlt className="mb-2 text-white" size={32}/>
-                <CiTwitter className="mb-2 text-white" size={32}/>
-                <CiLinkedin className="mb-2 text-white" size={32} />
-                <CiInstagram className="mb-2 text-white" size={32} />
+            <div className="hidden md:justify-center md:ml-5 md:mb-2 md:items-center md:w-11 md:flex-col md:flex md:bottom-0 md:left-0 md:fixed md:h-96">
+            <div className="flex flex-col items-end h-1/2">
+                <a href="https://www.github.com/afbelardi" target="_blank" title="Github"> 
+                    <VscGithubAlt className="mb-2 text-white" size={32}/>
+                </a>
+                <a href="https://www.twitter.com/afbelardi" target="_blank" title="Twitter">
+                    <CiTwitter className="mb-2 text-white" size={32}/>
+                </a>
+                <a href="https://www.linkedin.com/in/afbelardi" target="_blank" title="LinkedIn">
+                    <CiLinkedin className="mb-2 text-white" size={32} />
+                </a>
+                <a href="https://www.instagram.com/afbelardi" target="_blank" title="Instagram">
+                    <CiInstagram className="mb-2 text-white" size={32} />
+                </a>
+                <a href="/AlexBelardiResume.pdf" target="_blank" title="View Resume">
+                    <IoNewspaperOutline className="mb-2 text-white" size={28} />
+                </a>
             </div>
           <div className="flex-col mt-3">
             <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,4 @@
 import { useRef, useEffect, useState } from 'react';
-import { BiCaretUp } from 'react-icons/bi';
-import { VscGithubAlt } from 'react-icons/vsc';
-import { CiTwitter, CiLinkedin, CiInstagram } from 'react-icons/ci';
-import { SlSocialLinkedin, SlSocialInstagram } from 'react-icons/sl';
 import Navbar from '../components/Navbar';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -35,45 +31,13 @@ export default function Home(props) {
       <Intro />
       <AboutMe />
       <Projects />
+      <Sidebars />
       {isOpen ? 
         <Splash 
         />
         : ''
       }
       <Skills />
-      <div className="hidden md:w-13 md:bottom-0 md:flex-col md:right-0 md:flex md:items-center md:mb-2 md:mr-5 md:fixed md:h-96 md:">
-        <a className="text-lg text-blue-100 writing-vertical font-recoleta-light" href="mailto:afbelardicode@gmail.com">afbelardicode@gmail.com</a>
-        <BiCaretUp className="mt-2 mr-1 text-green-300"/>
-        <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-        <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-        <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-        <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-        <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-        <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-        <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-        <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-        <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-      </div>
-      <div className="hidden md:justify-center md:ml-5 md:items-center md:w-11 md:flex-col md:flex md:bottom-0 md:left-0 md:fixed md:h-96">
-          <div className="flex flex-col items-end h-2/5">
-          <VscGithubAlt className="mb-2 text-white" size={32}/>
-          <CiTwitter className="mb-2 text-white" size={32}/>
-          <CiLinkedin className="mb-2 text-white" size={32} />
-          <CiInstagram className="mb-2 text-white" size={32} />
-          </div>
-          <div className="flex-col mt-3">
-            <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-            <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-            <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-            <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-            <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-            <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-            <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-            <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-            <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-            <BiCaretUp className="mt-0.5 mr-1 text-green-300"/>
-        </div>
-      </div>
     </> 
   )
 }
