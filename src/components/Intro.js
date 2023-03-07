197d8ef9945b003c546d1bc915d43347a43bd0dc
afbelardi <afbelardi@gmail.com>
2023-03-07 11:51:53 -0500

    font sizing changes for md screens

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,10 +3,10 @@ import styles from '../styles/aboutme.module.css';
 export default function Intro() {
     return(
         <div className="flex flex-col w-full h-52 mt-14" id={styles.intro}>
-            <section className="flex flex-col w-full">
-                <h1 className="p-2 ml-6 text-lg text-white font-recoleta-regular">Hi, welcome to my portfolio.</h1>
-                <h1 className="mb-2 ml-8 text-2xl text-blue-200 font-recoleta-bold">My name is Alex Belardi.</h1>
-                <h1 className="w-4/5 ml-8 text-2xl text-blue-300 font-recoleta-bold">I'm passionate about turning cool and innovative ideas into web apps.</h1>
+            <section className="flex flex-col w-full md:ml-24">
+                <h1 className="p-2 ml-6 text-lg text-white md:text-xl font-recoleta-regular">Hi, welcome to my portfolio.</h1>
+                <h1 className="mb-2 ml-8 text-2xl text-blue-200 md:text-3xl font-recoleta-bold">My name is Alex Belardi.</h1>
+                <h1 className="w-4/5 ml-8 text-2xl text-blue-300 md:text-3xl font-recoleta-bold">I'm passionate about turning cool and innovative ideas into web apps.</h1>
             </section>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,9 @@ export default function Navbar(props) {
     return (
         <div className="z-0 flex justify-between h-1/8">
             <div className='z-40 p-5'>
-                <h1 className="-my-2 text-4xl text-white font-bebas">Alex</h1>
-                <h1 className="-my-2 text-4xl text-white font-bebas">Belardi</h1>
-                <h1 className="text-4xl text-white font-bebas">Code</h1>
+                <h1 className="-my-2 text-4xl text-white md:text-5xl font-bebas">Alex</h1>
+                <h1 className="-my-2 text-4xl text-white md:text-5xl font-bebas">Belardi</h1>
+                <h1 className="text-4xl text-white md:text-5xl font-bebas">Code</h1>
             </div>
             <div className="z-50 flex justify-center w-1/5 mt-8 md:hidden" >
                 <Hamburger toggled={props.isOpen} toggle={props.setIsOpen} color="white" size={40}/>
