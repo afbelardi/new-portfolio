555e47b1a4eaacfac600e10f92cdf0637ea09114
afbelardi <afbelardi@gmail.com>
2023-03-07 11:29:07 -0500

    updates for md size screens

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ export default function Projects(props) {
     const { ref: gtopRef, inView: isGtopVisible } = useInView()
     return (
         <div className="flex flex-col w-full p-4 mt-10 min-h-1000">
-            <div className="flex w-full">
+            <div className="flex w-full md:justify-center">
                 <h1 className="ml-2 text-4xl text-white font-recoleta-bold whitespace-nowrap" id={styles.about}>Projects I've built</h1>
                 <Fade right>
                     <hr className="w-2/5 h-0 mt-5 ml-7 bg-lighter-blue" />
