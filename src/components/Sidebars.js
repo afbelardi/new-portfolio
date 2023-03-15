4f23209791e2b797c1cd0170fdfb6ecd80b79842
afbelardi <afbelardi@gmail.com>
2023-03-15 16:42:17 -0400

    added github gists icon to sidebar. Removed instagram

--- src/components/Sidebars.js
+++ src/components/Sidebars.js
@@ -1,8 +1,8 @@
 import { BiCaretUp } from 'react-icons/bi';
 import { VscGithubAlt } from 'react-icons/vsc';
 import { CiTwitter, CiLinkedin, CiInstagram } from 'react-icons/ci';
-import { SlSocialLinkedin, SlSocialInstagram } from 'react-icons/sl';
 import { IoNewspaperOutline } from 'react-icons/io5';
+import { GoGist } from 'react-icons/go';
 
 export default function Sidebars() {
 
@@ -29,7 +29,10 @@ export default function Sidebars() {
             <div className="hidden md:justify-center md:ml-5 md:mb-2 lg:z-50 xl:ml-20 lg:absolute lg:ml-10 md:items-center md:w-11 md:flex-col md:flex md:bottom-0 md:left-0 md:fixed md:h-96">
             <div className="flex flex-col items-end h-1/2">
                 <a href="https://www.github.com/afbelardi" target="_blank" rel="noreferrer" title="Github"> 
-                    <VscGithubAlt className="mb-2 text-white" size={32}/>
+                    <VscGithubAlt className="mb-3 text-white" size={32}/>
+                </a>
+                <a href="https://gist.github.com/afbelardi" target="_blank" rel="noreferrer" title="Gists"> 
+                    <GoGist className="mb-2 text-white" size={32}/>
                 </a>
                 <a href="https://www.twitter.com/afbelardi" target="_blank" rel="noreferrer" title="Twitter">
                     <CiTwitter className="mb-2 text-white" size={32}/>
@@ -37,11 +40,8 @@ export default function Sidebars() {
                 <a href="https://www.linkedin.com/in/afbelardi" target="_blank" rel="noreferrer" title="LinkedIn">
                     <CiLinkedin className="mb-2 text-white" size={32} />
                 </a>
-                <a href="https://www.instagram.com/afbelardi" target="_blank" rel="noreferrer" title="Instagram">
-                    <CiInstagram className="mb-2 text-white" size={32} />
-                </a>
                 <a href="/AlexBelardiResume.pdf" target="_blank" rel="noreferrer" title="View Resume">
-                    <IoNewspaperOutline className="mb-2 text-white" size={28} />
+                    <IoNewspaperOutline className="mb-2 text-white" size={30} />
                 </a>
             </div>
           <div className="flex-col mt-3">
