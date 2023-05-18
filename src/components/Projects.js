35f4c71bea0f515110908971f41fdc622d48cfd7
afbelardi <afbelardi@gmail.com>
2023-05-18 15:44:33 -0400

    changes to project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -37,7 +37,7 @@ export default function Projects({ projectsRef }) {
                                         The site features a light and dark mode option as well as the ability to click on each photo and make them larger.
                                     </p>
                                 </section>
-                            <footer className="flex flex-col justify-end w-full h-1/6">
+                            <footer className="flex flex-col justify-end w-full lg:mt-6 h-1/6">
                                 <ul className="flex flex-wrap justify-start w-2/3 p-1 mb-2 ml-4 text-sm text-white whitespace-nowrap font-recoleta-bold">
                                     <li className="flex mr-2">
                                         <BiCaretRight className="mt-0.5 text-green-300"/>
@@ -81,7 +81,7 @@ export default function Projects({ projectsRef }) {
                                             3 photos with a smart contract I created, view the transactions on Etherscan and see the collection on OpenSea.
                                         </p>
                                 </section>
-                                <footer className="flex flex-col justify-end w-full h-1/6">
+                                <footer className="flex flex-col justify-end w-full lg:mt-6 h-1/6">
                                 <ul className="flex flex-wrap justify-start w-2/3 p-1 mb-2 ml-4 text-sm text-white whitespace-nowrap font-recoleta-bold">
                                     <li className="flex mr-2">
                                         <BiCaretRight className="mt-0.5 text-green-300"/>
@@ -146,17 +146,17 @@ export default function Projects({ projectsRef }) {
                         </div>
                 </div>
                 <div ref={npRef} className={`${styles.notanimated} ${isNpVisible ? styles.projects: ''}`}>
-                        <img className="object-cover rounded-lg opacity-15 min-h-400"  src="/photomint.png" />
-                        <div className="absolute top-0 z-40 w-full h-full rounded-lg opacity-90 bg-light-navy">
+                <img className="object-cover rounded-lg opacity-15 min-h-400"  src="/gtop.png" />
+                        <div className="absolute top-0 z-40 w-full h-full rounded-lg bg-light-navy opacity-90">
                             <div className="flex flex-col items-center justify-center w-full h-full">
-                                <section className="flex justify-between w-5/6 p-3 ">
-                                    <VscFolderOpened size={40} className="mr-2 text-blue-300 min-w-40" />
-                                    <h1 className="mt-2 text-2xl text-blue-400 whitespace-nowrap font-recoleta-bold font-outline">National Park Finder</h1>
+                                <section className="flex justify-between w-5/6 p-3">
+                                    <VscFolderOpened size={40} className="text-blue-300 min-w-40"/>
+                                    <h1 className="mt-2 text-2xl text-blue-400 min-w-215 font-recoleta-bold font-outline">National Park Finder</h1>
                                 </section>
-                                <section className='w-full p-4 mt-5 mb-4 text-center text-white md:-mb-6 h-36 font-recoleta-regular'>
-                                        <p>Currently a work in progress. A site that aggregates all National Park data into one site. Users can create an account, view
-                                            info about parks to help them plan a trip and add parks to a list of favorites.
-                                        </p>
+                                <section className='w-full p-4 mt-5 text-center text-white md:-mb-6 h-36 font-recoleta-regular'>
+                                    <p>A work in progress. The site aggregates data about US National Park sites thereby helping users plan trips by state. Users are able 
+                                        to create an account and add parks to a list of favorites.
+                                    </p>
                                 </section>
                                 <footer className="flex flex-col justify-end w-full h-1/6">
                                 <ul className="flex flex-wrap justify-start w-2/3 p-1 mb-2 ml-4 text-sm text-white whitespace-nowrap font-recoleta-bold">
@@ -170,29 +170,25 @@ export default function Projects({ projectsRef }) {
                                     </li>
                                     <li className="flex mr-2">
                                         <BiCaretRight className="mt-0.5 text-green-300"/>
-                                        <p>Tailwind CSS</p>
-                                    </li>
-                                    <li className="flex mr-2">
-                                        <BiCaretRight className="mt-0.5 text-green-300"/>
                                         <p>JWT</p>
                                     </li>
                                     <li className="flex mr-2">
                                         <BiCaretRight className="mt-0.5 text-green-300"/>
-                                        <p>Google Maps API</p>
+                                        <p>MongoDB</p>
+                                    </li>
+                                    <li className="flex mr-2">
+                                        <BiCaretRight className="mt-0.5 text-green-300"/>
+                                        <p>Tailwind CSS</p>
                                     </li>
                                 </ul>
                             </footer>
-                            <div className="flex justify-start w-full pb-2 pl-6 mb-2">
+                            <div className="flex justify-start w-full pl-6 mb-2 min-w-200">
                                 <a href="https://github.com/afbelardi/np-frontend" target="_blank" rel="noreferrer">
                                     <FiGithub size={28} className="mt-5 text-white"/>
                                 </a>
-                                {/* <a href="https://test-mint-site.herokuapp.com/" target="_blank" rel="noreferrer">
-                                    <ImLink size={28} className="mt-5 ml-3 text-white" />
-                                </a> */}
                             </div>
                             </div>
-                        </div>
-                             
+                        </div> 
                 </div>
                 <div ref={npBackendRef} id={styles.thirdgrid} className={`${styles.notanimated} ${isBackendVisible ? styles.projects: ''}`}>
                         <img className="object-cover rounded-lg opacity-15 min-h-400"  src="/photomint.png" />
@@ -202,7 +198,7 @@ export default function Projects({ projectsRef }) {
                                     <VscFolderOpened size={40} className="text-blue-300" />
                                     <h1 className="mt-2 text-2xl text-blue-400 font-recoleta-bold font-outline">NP Finder Backend</h1>
                                 </section>
-                                <section className='w-full p-4 mt-5 text-center text-white md:-mb-6 h-36 font-recoleta-regular'>
+                                <section className='w-full p-4 mt-5 text-center text-white lg:w-5/6 md:-mb-6 h-36 font-recoleta-regular'>
                                         <p>Currently a work in progress. The backend to my National Park Finder project. Incorporates signup and login functionality with JWT and MongoDB. Makes calls
                                             to the official National Parks API.
                                         </p>
